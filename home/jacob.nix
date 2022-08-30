0a474a43cf5a1aa04f52fd234905e2139a18f668
Jacob Hinkle <jacob.hinkle@jhink.org>
2022-08-30 14:39:55 -0400

    Add hostname to tmux status-left

--- home/jacob.nix
+++ home/jacob.nix
@@ -152,36 +152,7 @@
         };
       };
     };
-    tmux = {
-      enable = true;
-      aggressiveResize = true;
-      clock24 =  true;
-      escapeTime = 0;
-      historyLimit = 10000;
-      keyMode = "vi";
-      shortcut = "a";
-      terminal = "kitty";
-      extraConfig = ''
-        unbind [
-        bind Escape copy-mode
-
-        bind f set-option status
-
-        bind c new-window -c "#{pane_current_path}"
-
-        # Set status bar
-        set -g status-position bottom
-        set -g status-style fg=white
-        set -g status-left '#[fg=green]#(who -s | awk "{ print \$1}" | sort -u | grep -v 4jh | xargs echo) #[fg=black,bright]#H'
-        # TODO: highlighted for nested local session as well
-        wg_is_keys_off="#[fg=red,bright]#([ $(tmux show-option -qv key-table) = 'off' ] && echo 'PASSTHRU')#[default]"
-        set -g status-right "#[fg=yellow,bright]#S $wg_is_keys_off #[fg=green,dim]%H:%M"
-        set-window-option -g window-status-current-style fg=red
-        # align center the window list
-        set -g status-justify centre
-        
-      '';
-    };
+    tmux = import ./tmux.nix;
     zsh = {
       enable = true;
       enableSyntaxHighlighting = true;
---
+++ home/tmux.nix
@@ -0,0 +1,31 @@
+{
+  enable = true;
+  aggressiveResize = true;
+  clock24 =  true;
+  escapeTime = 0;
+  historyLimit = 10000;
+  keyMode = "vi";
+  shortcut = "a";
+  terminal = "kitty";
+  extraConfig = ''
+    unbind [
+    bind Escape copy-mode
+
+    bind f set-option status
+
+    bind c new-window -c "#{pane_current_path}"
+
+    # Set status bar
+    set -g status-position bottom
+    set -g status-style fg=white
+    set -g status-left '#h'
+    set -g status-left-style 'fg=blue'
+    # TODO: highlighted for nested local session as well
+    wg_is_keys_off="#[fg=red,bright]#([ $(tmux show-option -qv key-table) = 'off' ] && echo 'PASSTHRU')#[default]"
+    set -g status-right "#[fg=yellow,bright]#S $wg_is_keys_off #[fg=green,dim]%H:%M"
+    set-window-option -g window-status-current-style fg=red
+    # align center the window list
+    set -g status-justify centre
+    
+  '';
+}
