bb6c5f39cccf37368c6f2af6270739d37c51840c
Jacob Hinkle <jacob.hinkle@jhink.org>
2022-09-08 12:05:07 -0400

    Clean up flake to only have one nixpkgs dependency

--- flake.nix
+++ flake.nix
@@ -3,13 +3,25 @@
 
   inputs = {
     nixpkgs.url = github:nixos/nixpkgs/nixos-22.05;
-    nixos-hardware.url = github:nixos/nixos-hardware/master;
+    nixos-hardware = {
+      url = github:nixos/nixos-hardware/master;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
-      #url = "github:nix-community/home-manager";
       url = "https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    sops-nix.url = github:Mic92/sops-nix;
+    sops-nix = {
+      url = github:Mic92/sops-nix;
+      # sops-nix uses both -22.05 and -unstable in their flake.
+      # As far as I can tell, 22.05 is only used for testing, whereas unstable
+      # is used for the tooling. So here, I let both of these follow our
+      # nixpkgs input. Note that after NixOS releases, this might break since
+      # they may do away with 22.05 at that point.
+      # https://github.com/Mic92/sops-nix/blob/master/flake.nix
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs-22_05.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ { self, nixpkgs, nixos-hardware, home-manager, sops-nix, ... }:
